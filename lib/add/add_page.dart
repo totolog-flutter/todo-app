8382c9f625990a280192577e606e18b636a608f8
totolog <totolog1126@gmail.com>
2020-11-28 22:32:38 +0900

    Add function of delete tasks

--- lib/add/add_page.dart
+++ lib/add/add_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'package:todo_app/add/add_model.dart';
 import 'package:todo_app/main-model.dart';
 
 class AddPage extends StatelessWidget {
